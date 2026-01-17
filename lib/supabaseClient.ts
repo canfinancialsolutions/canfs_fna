b47df39ed7d069f4c6411bcd2ba6175883167777
canfinancialsolutions <canfinancialsolutions@gmail.com>
2026-01-16 18:01:34 -0600

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 // lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "supabase/supabase-js";
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
